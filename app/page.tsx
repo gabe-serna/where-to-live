1e520e20e5a9307182cb037e9ccabab9b33cee96
Gabe Serna <gabriel.serna118@gmail.com>
2024-12-01 10:54:58 -0500

    Attempt to Add Map Model

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-2">
       <h1 className="text-5xl text-gray-50">Where to Live</h1>
-      <p className="text-gray-400">
+      <p className="mb-4 text-gray-400">
         Find the perfect place in world to live based on your preferences.
       </p>
       <RoutingButton route="map">View Map</RoutingButton>
--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,16 +1,18 @@
 "use client";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+// import { WorldMap } from "./WorldMap";
+import { WorldMap2 } from "./WorldMap2";
 
 export default function Experience() {
   return (
     <Canvas style={{ height: "100vh" }}>
       <OrbitControls />
 
-      <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
-      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight intensity={1} position={[10, 10, 5]} />
+
+      <WorldMap2 />
     </Canvas>
   );
 }
