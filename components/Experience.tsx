2b343478802cb68fe8c7cc549947beb3ec625fb2
Gabe Serna <gabriel.serna118@gmail.com>
2024-12-02 15:30:08 -0500

    Add Map

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -2,17 +2,30 @@
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 // import { WorldMap } from "./WorldMap";
-import { WorldMap2 } from "./WorldMap2";
+import { WorldMap } from "./WorldMap";
+import {
+  EffectComposer,
+  Outline,
+  Select,
+  Selection,
+} from "@react-three/postprocessing";
 
 export default function Experience() {
   return (
     <Canvas style={{ height: "100vh" }}>
       <OrbitControls />
 
+      <Selection>
+        <EffectComposer>
+          <Outline blur edgeStrength={100} />
+        </EffectComposer>
+        <Select enabled>
+          <WorldMap />
+        </Select>
+      </Selection>
+
       <ambientLight intensity={0.5} />
       <directionalLight intensity={1} position={[10, 10, 5]} />
-
-      <WorldMap2 />
     </Canvas>
   );
 }
